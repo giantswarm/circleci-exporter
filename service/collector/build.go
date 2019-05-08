ee8408e9bf5233a95fbf0af0ae0411af91361fc9
jihyuk <jhbok@giantswarm.io>
2019-05-08 16:32:26 +0200

    implement histogram & gauge

--- service/collector/build.go
+++ service/collector/build.go
@@ -7,6 +7,44 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+var (
+	buildLabelDesc *prometheus.Desc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, subsystem, "build"),
+		"circle CI build labels.",
+		[]string{
+			labelBranch,
+			labelRepo,
+			labelState,
+		},
+		nil,
+	)
+)
+
+var (
+	buildDesc = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    prometheus.BuildFQName(namespace, subsystem, "labels_buildtime"),
+			Help:    "circle CI build time duration.",
+			Buckets: prometheus.ExponentialBuckets(bucketStart, bucketFactor, numBuckets),
+		},
+		[]string{
+			labelBranch,
+			labelRepo,
+			labelState,
+		},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(buildDesc)
+}
+
+type key struct {
+	Branch string
+	Repo   string
+	Status string
+}
+
 // BuildConfig is this collector's configuration struct.
 type BuildConfig struct {
 	CircleCIClient *circleci.Client
@@ -38,12 +76,46 @@ func NewBuild(config BuildConfig) (*Build, error) {
 
 // Collect is the main metrics collection function.
 func (c *Build) Collect(ch chan<- prometheus.Metric) error {
-	// TODO
+	builds, err := c.circleCIClient.ListRecentBuilds(100, 0)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	repoCount := map[key]int{}
+	for _, build := range builds {
+		branch := build.Branch
+		repo := build.Reponame
+		status := build.Status
+
+		buildDesc.WithLabelValues(branch, repo, status).Observe(float64(*build.BuildTimeMillis))
+
+		{
+			k := key{
+				Branch: build.Branch,
+				Repo:   build.Reponame,
+				Status: build.Status,
+			}
+			repoCount[k] = repoCount[k] + 1
+		}
+
+	}
+
+	for k, v := range repoCount {
+		ch <- prometheus.MustNewConstMetric(
+			buildLabelDesc,
+			prometheus.GaugeValue,
+			float64(v),
+			k.Branch,
+			k.Repo,
+			k.Status,
+		)
+	}
+
 	return nil
 }
 
 // Describe emits the description for the metrics collected here.
 func (c *Build) Describe(ch chan<- *prometheus.Desc) error {
-	// TODO
+	ch <- buildLabelDesc
 	return nil
 }
--- service/collector/collector.go
+++ service/collector/collector.go
@@ -1,1 +1,18 @@
 package collector
+
+const (
+	namespace = "circleci_exporter"
+	subsystem = "ci"
+)
+
+const (
+	labelBranch = "branch"
+	labelRepo   = "repo"
+	labelState  = "state"
+)
+
+const (
+	bucketStart  = 15000
+	bucketFactor = 2
+	numBuckets   = 8
+)
